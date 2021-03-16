0aa75fc5989443222e2c03c32d5dd065ec429d7d
Fadly Fatur <39433329+FadlyFatur@users.noreply.github.com>
2021-03-16 10:56:22 +0700

    detail berita

--- resources/views/landing/detail-berita.blade.php
+++ resources/views/landing/detail-berita.blade.php
@@ -5,22 +5,27 @@
 @endsection
 
 @section('content')
-    <div class="VIRTUAL-LAB-ITENAS">
-        <div class="VIRTUAL-LAB-ITENAS_img">
-            <img src="tesla.jpg" alt="">
-        </div>
-        <div class="VIRTUAL-LAB-ITENAS_info">
-            <div class="VIRTXUAL-LAB-ITENAS_date">
-                <span>Selasa</span>
-                <span>15 maret 2021</span>
-            </div>
-            <h1 class="VIRTUAL-LAB-ITENAS_title">Investor Tesla Gugat Elon Musk, Mengapa?</h1>
-            <p class="VIRTUAL-LAB-ITENAS_text"> 
-            CEO Tesla Elon Musk dan dewan perusahaan mobil listrik Tesla secara resmi digugat pemegang saham 
-            karena penggunaan Twitter.
-            </p>
-            <a href="#" class="VIRTUAL-LAB-ITENAS_cta">read more</a>
-        </div>
 
+    <div class="VIRTUAL-LAB-ITENAS container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="VIRTUAL-LAB-ITENAS_img">
+                    <img src="tesla.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="VIRTUAL-LAB-ITENAS_info">
+                    <div class="VIRTXUAL-LAB-ITENAS_date">
+                        <span>Selasa</span>
+                        <span>15 maret 2021</span>
+                    </div>
+                    <h1 class="VIRTUAL-LAB-ITENAS_title">Investor Tesla Gugat Elon Musk, Mengapa?</h1>
+                    <p class="VIRTUAL-LAB-ITENAS_text"> 
+                    CEO Tesla Elon Musk dan dewan perusahaan mobil listrik Tesla secara resmi digugat pemegang saham 
+                    karena penggunaan Twitter.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>    
 @endsection
